8f291cbb1c032f306837af0fef1a49a6ae8484da
gabrielsouza95 <alves95gabriel@gmail.com>
2025-05-07 02:28:43 -0300

    refactor: now the method replaces the letters on a empty string (trying to make the string occupy the space so it didn't flicker the page scroll position)

--- dist/src/TypingEffect.js
+++ dist/src/TypingEffect.js
@@ -1,9 +1,11 @@
 "use strict";
 
 class TypingEffect {
-    constructor(elementId, textToType, typingSpeed, iterations_offset, domDocument, windowElement) {
+    constructor(elementId, textToType, baseString, typingSpeed, iterations_offset, domDocument, windowElement) {
         this.elementId = elementId;
         this.textToType = textToType;
+        this.baseString = baseString;
+        this.originalBaseString = baseString;
         this.typingSpeed = typingSpeed;
         this.domDocument = domDocument;
         this.windowElement = windowElement;
@@ -19,7 +21,11 @@ class TypingEffect {
             console.log(this.textToType.length);
             console.log(this.iteration);
             if(this.iteration < this.textToType.length) {
-                this.domDocument.getElementById(this.elementId).innerHTML += this.textToType.charAt(this.iteration);
+                this.domDocument.getElementById(this.elementId).innerHTML = this.baseString = this.setCharAt(
+                    this.baseString,
+                    this.iteration,
+                    this.textToType.charAt(this.iteration)
+                );
                 this.iteration++;
             }
             if( this.iteration >= this.textToType.length && this.iteration < this.textToType.length + this.iterations_offset){
@@ -27,11 +33,17 @@ class TypingEffect {
             }           
             if(this.iteration >= this.textToType.length + this.iterations_offset){
                 await this.pause(10);
-                this.domDocument.getElementById(this.elementId).innerHTML = '';
+                this.domDocument.getElementById(this.elementId).innerHTML = this.originalBaseString;
+                this.baseString = this.originalBaseString;
                 this.iteration = 0;
             }
             setTimeout(()=>{this.typeWriter()}, this.typingSpeed);
         }
+    }
+
+    setCharAt(str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substring(0,index) + chr + str.substring(index+1);
     }
 }
 
